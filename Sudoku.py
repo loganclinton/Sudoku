b621651d17bdcbc27ee51d09760256b4650d2503
loganclinton <60623244+loganclinton@users.noreply.github.com>
2020-09-16 17:47:23 -0400

    Add files via upload

--- Sudoku.py
+++ Sudoku.py
@@ -7,6 +7,15 @@ window.title("Sudoku")
 window.minsize(200, 250)
 window.resizable(width=False, height=False)
 
+puzzle = [0, 6, 0, 0, 1, 0, 9, 0, 0,
+          3, 0, 1, 0, 4, 0, 2, 0, 5,
+          4, 0, 5, 7, 2, 8, 6, 0, 0,
+          0, 0, 0, 2, 0, 0, 0, 0, 0,
+          0, 0, 8, 6, 9, 5, 3, 0, 0,
+          0, 0, 0, 0, 0, 1, 0, 0, 0,
+          0, 0, 4, 8, 5, 9, 1, 0, 7,
+          5, 0, 7, 0, 6, 0, 4, 0, 3,
+          0, 0, 9, 0, 7, 0, 0, 6, 0]
 
 def solve():
     # Backtracking algorithm
@@ -19,12 +28,18 @@ button.place(x = 70, y = 205)
 column = 1
 row = 1
 for i in range(81):
-    block = ttk.Entry(window, padx = 10, pady = 10)
+    block = ttk.Entry(window, width = 3)
     block.grid(column = column, row = row)
+    if (puzzle[i] != 0):
+        block.insert(tk.INSERT, puzzle[i])
+    else:
+        print("Done.")
     row += 1
     if (row > 9):
         row = 1
     if ((i+1) % 9 == 0):
         column += 1
+
+        
 
 window.mainloop()
